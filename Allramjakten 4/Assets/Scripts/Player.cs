0a39e78a9d11af2596bd923f068f40a684876098
Liljan <marcus.t.lilja@gmail.com>
2017-02-07 19:59:40 +0100

    Player Movement.

--- Allramjakten 4/Assets/Scripts/Player.cs
+++ Allramjakten 4/Assets/Scripts/Player.cs
@@ -2,15 +2,57 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // movement
+    private Rigidbody2D rb2d;
+    public float speed = 5.0f;
+
+    private float vx, vy;
+
+    // Use this for initialization
+    void Start()
+    {
+        vx = 0.0f;
+        vy = 0.0f;
+
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // go left
+        if (Input.GetButtonDown("Left"))
+        {
+            vx = -speed;
+            vy = 0.0f;
+        }
+        else if (Input.GetButtonDown("Right"))
+        {
+            vx = speed;
+            vy = 0.0f;
+
+        }
+        else if (Input.GetButtonDown("Up"))
+        {
+            vx = 0.0f;
+            vy = speed;
+
+        }
+        else if (Input.GetButtonDown("Down"))
+        {
+            vx = 0.0f;
+            vy = -speed;
+        }
+
+        transform.Translate(vx * Time.deltaTime, vy * Time.deltaTime, 0.0f);
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+
+    }
 }
